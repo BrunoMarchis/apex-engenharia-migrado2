a846f7cd1274277667405808119290e95eb486a5
Bruno Marchis <90621044+BrunoMarchis@users.noreply.github.com>
2026-06-08 16:38:00 -0300

    Update server.js

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,12 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 const db = require("./db");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-
-const cors = require("cors");
 
 app.get("/", (req,res)=>{
  res.json({status:"online"});
