522fec0c78f63c8faf2db3a934922740b58d6575
Bruno Marchis <90621044+BrunoMarchis@users.noreply.github.com>
2026-06-08 16:28:00 -0300

    Update server.js

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -3,7 +3,12 @@ const db = require("./db");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
+
+const express = require("express");
+const cors = require("cors");
+const db = require("./db");
 
 app.get("/", (req,res)=>{
  res.json({status:"online"});
@@ -39,3 +44,4 @@ app.get("/api/clientes", async (req,res)=>{
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, ()=> console.log("Servidor rodando"));
+
