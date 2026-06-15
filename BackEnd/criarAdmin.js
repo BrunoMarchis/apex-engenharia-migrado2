819c22a810dcc01e5811f91ec27d4c001f1ae76f
Bruno Marchis <90621044+BrunoMarchis@users.noreply.github.com>
2026-06-15 16:40:47 -0300

    Implement admin user creation functionality
    This function creates an admin user by hashing the password and inserting the user into the database.

--- BackEnd/criarAdmin.js
+++ BackEnd/criarAdmin.js
@@ -1,1 +1,34 @@
+const bcrypt = require("bcrypt");
+const db = require("./db");
 
+async function criarAdmin() {
+
+    try {
+
+        const usuario = "bruno";
+        const senha = "Apex@2026";
+
+        const senhaHash = await bcrypt.hash(
+            senha,
+            10
+        );
+
+        await db.query(
+            `INSERT INTO administradores
+            (usuario, senha)
+            VALUES ($1, $2)`,
+            [usuario, senhaHash]
+        );
+
+        console.log("Administrador criado com sucesso!");
+
+    } catch (erro) {
+
+        console.error("ERRO COMPLETO:");
+        console.error(erro);
+
+    }
+
+}
+
+criarAdmin();
